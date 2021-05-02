dc1b9778ac8f1ed020c4e008e5002d3b114f49a1
Vedran Milković <vedran.milkovic25@gmail.com>
2021-05-02 20:44:26 +0200

    Listen to krenel event

--- src/Repository/ConferenceRepository.php
+++ src/Repository/ConferenceRepository.php
@@ -19,6 +19,10 @@ class ConferenceRepository extends ServiceEntityRepository
         parent::__construct($registry, Conference::class);
     }
 
+    public function findAll()
+    {
+        return $this->findBy([], ['year' => 'ASC', 'city' => 'ASC']);
+    }
     // /**
     //  * @return Conference[] Returns an array of Conference objects
     //  */
